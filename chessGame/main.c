c9bcee69186b2ee9b34a73d890769d6d98d6045a
Kader Ndoye <kader.ndoye@epita.fr>
2021-03-31 19:12:32 +0200

    push

--- chessGame/main.c
+++ chessGame/main.c
@@ -12,7 +12,24 @@ int main()
     {
         if(i%8 == 0)
             printf("\n");
-        printf("%i | ",bd[i].type);
+
+	if(bd[i].type == 1)
+	    printf("P | ");
+
+	if(bd[i].type == 2)
+	    printf("R | ");
+
+	if(bd[i].type == 3)
+	    printf("B | ");
+
+	if(bd[i].type == 4)
+	    printf("k | ");
+
+	if(bd[i].type == 5)
+	    printf("Q | ");
+
+	if(bd[i].type == 6)
+	    printf("K | ");
     }
     printf("Hey !");
 
