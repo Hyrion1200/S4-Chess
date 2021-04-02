db1f9314af325c9dfbd23a79a57020b71342f608
de Souza <nathan.de-souza@epita.fr>
2021-04-02 13:46:42 +0200

    visual template

---
+++ chessGame/visual.c
@@ -0,0 +1,16 @@
+#include "piece.h"
+
+
+void updateSquare(Piece* board, int start, int end)
+{
+    //TODO
+    //update board[start] and board[end]
+}
+
+void printBoard(Piece* board)
+{
+    //TODO
+    //print board regarding piece type and piece color
+    //Ex: P (Pawn White) // p (Pawn Black)
+}
+
---
+++ chessGame/visual.h
@@ -0,0 +1,8 @@
+#ifndef VISUAL_H
+#define VISUAL_H
+
+void updateSquare(Piece* board, int start, int end);
+
+void printBoard(Piece* board);
+
+#endif
