1d67f24023ee3db0de1b343afcb1984386e450a9
de Souza <nathan.de-souza@epita.fr>
2021-04-02 14:08:32 +0200

    moves init

---
+++ chessGame/moves.c
@@ -0,0 +1,12 @@
+#include "moves.h"
+
+Move initMove()
+{
+    Move null =
+    {
+        .start = 0;
+        .end = 0;
+        .value = 0;
+    }
+    return null;
+}
---
+++ chessGame/moves.h
@@ -0,0 +1,15 @@
+#ifndef MOVES_H
+#define MOVES_H
+
+typedef struct Move
+{
+    int start;
+    int end;
+    int value;
+}Move
+
+Move initMove();
+
+
+
+#endif
